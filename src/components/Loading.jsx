dc1e06e35ca4a6684751197b4c6eb838b9188ef6
Rolando <rolandohernandezleal@gmail.com>
2025-10-18 22:42:53 -0300

    disable game on ios with css

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -175,7 +175,7 @@ export default function Loading({ onComplete }) {
             className="language-button"
             onClick={() => handleSelectLanguage("en")}
           >
-            English!
+            English
           </button>
           <button
             className="language-button"
